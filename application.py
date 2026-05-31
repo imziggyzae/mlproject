d7f92b1b1390a52aa9fb384117d3fcdadfc9229f
imziggyzae <zaeduren@gmail.com>
2026-05-31 06:10:30 +0000

    fix: update project root path and add Procfile

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from flask import Flask, render_template, request
 
-project_root = Path(__file__).resolve().parents[1]
+project_root = Path(__file__).resolve().parent
 if str(project_root) not in sys.path:
     sys.path.insert(0, str(project_root))
 
